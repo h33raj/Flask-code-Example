f5c5432ba3687b5e4f549393595def1d323e0cf6
Heeraj <heeraj123@gmail.com>
2017-07-06 14:27:21 +0530

    Fixed a bug

--- app.py
+++ app.py
@@ -18,6 +18,32 @@ app.config['UPLOAD_FOLDER'] = 'uploads/'
 #Extensions which are accepted to be uploaded
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'pdf'])
 
+"""
+This function is where you store all your input validation controls. 
+It makes it easy to maintain whenever you want to apply changes for 
+certain input validation roles and reduces the chance of mistakes in your regexes.
+"""
+
+class validate(object):
+    def __init__(self):
+        self.pattern = ""
+
+    def inputValidation(self, type, value, level):
+        switcher = {
+            "alphanumeric": "^[a-zA-Z0-9]+$",
+            "nummeric": "^[0-9]*$",
+            "bool": "^(true|false)$"
+        }
+        self.pattern = switcher.get(type, "nothing")
+        match = re.findall(self.pattern, value)
+        if match:
+            return True
+        else:
+            raise Exception("User supplied value not in the range " + range)
+
+
+
+
 #Check whether the file can be uploaded
 def allowed_file(filename):
     return '.' in filename and \
